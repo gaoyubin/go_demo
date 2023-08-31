f3a474d2ad39462a536fdc2b712503f55cc4d9c3
appleybgao <appleybgao@tencent.com>
2023-08-31 15:01:57 +0800

    --log=

---
+++ sql_demo/go.mod
@@ -0,0 +1,8 @@
+module sqldemo
+
+go 1.16
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+)
---
+++ sql_demo/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "database/sql"
+
+var db *sql.DB
+
+func init() {
+	dsn := "user:password@tcp(127.0.0.1:3306/user"
+}
+func main() {
+
+}
